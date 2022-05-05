06d764d74987cf90814ac0712bf8a81251ebd974
Mohammad Ahamad <mahamad@flexpay.co.za>
2022-05-05 16:46:32 +0200

    API Integration - High Value Payment https://fnds3000.atlassian.net/browse/FLEXIT-16657 	new file:   .gitignore 	new file:   build.gradle 	new file:   gradlew 	new file:   settings.gradle 	new file:   src/main/java/za/co/flexpay/funding/FlexpayFundingApplication.java 	new file:   src/main/java/za/co/flexpay/funding/api/config/RestClientConfiguration.java 	new file:   src/main/java/za/co/flexpay/funding/api/constant/ApplicationConstants.java 	new file:   src/main/java/za/co/flexpay/funding/api/controller/SwiftFundingApi.java 	new file:   src/main/java/za/co/flexpay/funding/api/model/SwiftFundingBeneficiaryInfoDTO.java 	new file:   src/main/java/za/co/flexpay/funding/api/model/SwiftFundingPayerInfoDTO.java 	new file:   src/main/java/za/co/flexpay/funding/api/model/SwiftFundingResponse.java 	new file:   src/main/java/za/co/flexpay/funding/api/model/SwiftInFundingRequest.java 	new file:   src/main/java/za/co/flexpay/funding/api/wsclient/RestTemplateResponseErrorHandler.java 	new file:   src/main/java/za/co/flexpay/funding/api/wsclient/SwiftFundingServiceClient.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/za/co/flexpay/funding/FlexpayFundingApplicationTests.java

---
+++ src/main/java/za/co/flexpay/funding/api/model/SwiftFundingResponse.java
@@ -0,0 +1,42 @@
+package za.co.flexpay.funding.api.model;
+
+import java.io.Serializable;
+
+public class SwiftFundingResponse implements Serializable {
+
+    private String responseCode;
+    private String responseMessage;
+    private String transactionIdentifier;
+
+    public SwiftFundingResponse() {
+    }
+
+    public SwiftFundingResponse(String responseCode, String responseMessage) {
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getTransactionIdentifier() {
+        return transactionIdentifier;
+    }
+
+    public void setTransactionIdentifier(String transactionIdentifier) {
+        this.transactionIdentifier = transactionIdentifier;
+    }
+}
