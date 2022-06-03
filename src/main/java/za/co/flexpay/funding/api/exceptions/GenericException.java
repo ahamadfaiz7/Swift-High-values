cebb56fe05ed873e77857c6da435895ba844a767
Mohammad Ahamad <mahamad@flexpay.co.za>
2022-06-03 17:43:10 +0200

    Configure HTTP Basic Authentication for HVC https://fnds3000.atlassian.net/browse/FLEXIT-17031 	modified:   src/main/java/za/co/flexpay/funding/api/controller/SwiftFundingApi.java 	new file:   src/main/java/za/co/flexpay/funding/api/exceptions/GenericException.java 	new file:   src/main/java/za/co/flexpay/funding/api/exceptions/UnauthorisedException.java 	modified:   src/main/java/za/co/flexpay/funding/api/model/SwiftFundingPayerInfoDTO.java 	modified:   src/main/java/za/co/flexpay/funding/api/model/SwiftFundingResponse.java 	new file:   src/main/java/za/co/flexpay/funding/api/service/AuthenticationService.java 	modified:   src/main/java/za/co/flexpay/funding/api/wsclient/SwiftFundingServiceClient.java 	modified:   src/main/resources/application-dev.properties 	modified:   src/main/resources/application-preprod.properties 	modified:   src/main/resources/application-prod.properties 	modified:   src/main/resources/application.properties

---
+++ src/main/java/za/co/flexpay/funding/api/exceptions/GenericException.java
@@ -0,0 +1,14 @@
+package za.co.flexpay.funding.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 401 HTTP ERROR code to be returned if authorization fails
+ */
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class GenericException extends Exception {
+    public GenericException(String msg) {
+        super(msg);
+    }
+}
