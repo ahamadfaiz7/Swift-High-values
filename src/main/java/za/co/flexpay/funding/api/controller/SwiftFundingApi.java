f026cc6ab84db971748405f0728d0e0b617ccfb1
Mohammad Ahamad <mahamad@flexpay.co.za>
2022-06-06 16:52:35 +0200

    Configure HTTP Basic Authentication for HVC https://fnds3000.atlassian.net/browse/FLEXIT-17031 	modified:   src/main/java/za/co/flexpay/funding/api/controller/SwiftFundingApi.java

--- src/main/java/za/co/flexpay/funding/api/controller/SwiftFundingApi.java
+++ src/main/java/za/co/flexpay/funding/api/controller/SwiftFundingApi.java
@@ -1,10 +1,8 @@
 package za.co.flexpay.funding.api.controller;
 
-import org.apache.catalina.filters.HttpHeaderSecurityFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
