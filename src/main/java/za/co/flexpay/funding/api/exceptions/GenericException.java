84db70955b329f864210dbccd5254037ca557bc1
Mohammad Ahamad <mahamad@flexpay.co.za>
2022-06-06 11:17:05 +0200

    Code comments change https://fnds3000.atlassian.net/browse/FLEXIT-16953 	modified:   src/main/java/za/co/flexpay/funding/api/exceptions/GenericException.java

--- src/main/java/za/co/flexpay/funding/api/exceptions/GenericException.java
+++ src/main/java/za/co/flexpay/funding/api/exceptions/GenericException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * 500 HTTP ERROR code to be returned if authorization fails
+ * 500 HTTP ERROR code to be returned if a generic Exception is thrown.
  */
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class GenericException extends Exception {
