4fde29f32e2e5ac10509d1fecca14230328a8218
Mohammad Faiz Ahamad <mohammad.ahamad@paymentology.com>
2022-10-16 02:19:32 +0200

    Create Auth to access Old stack REST services from new Stack https://paymentology.atlassian.net/browse/FLEXIT-18036 	modified:   src/main/java/za/co/flexpay/funding/api/wsclient/SwiftFundingServiceClient.java 	modified:   src/main/resources/application-dev.properties 	modified:   src/main/resources/application-preprod.properties 	modified:   src/main/resources/application-prod.properties 	modified:   src/main/resources/application.properties

--- src/main/java/za/co/flexpay/funding/api/wsclient/SwiftFundingServiceClient.java
+++ src/main/java/za/co/flexpay/funding/api/wsclient/SwiftFundingServiceClient.java
@@ -6,8 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import za.co.flexpay.funding.api.config.RestClientConfiguration;
 import za.co.flexpay.funding.api.model.SwiftFundingResponse;
 import za.co.flexpay.funding.api.model.SwiftInFundingRequest;
 
@@ -21,6 +24,14 @@ public class SwiftFundingServiceClient {
 
     @Value("${flexpay.customer-api.service.url}")
     private String otpServiceUrl;
+    @Value("${flexPay.funding.old.stack.api.username}")
+    private String flexPayFundingUser;
+    @Value("${flexPay.funding.old.stack.api.password}")
+    private String flexPayFundingPassword;
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
 
     /**
      * @param swiftInFundingRequest
@@ -29,12 +40,24 @@ public class SwiftFundingServiceClient {
     public SwiftFundingResponse swiftFundAccount(SwiftInFundingRequest swiftInFundingRequest) {
 
         SwiftFundingResponse swiftFundingResponse = null;
+        restTemplate = restTemplateBuilder
+                .basicAuthentication(flexPayFundingUser, flexPayFundingPassword)
+                .build();
         try {
             LOG.info("Making API call to-> "+ otpServiceUrl);
             swiftFundingResponse = restTemplate.postForObject(otpServiceUrl, swiftInFundingRequest, SwiftFundingResponse.class);
             LOG.info("API call to "+ otpServiceUrl+ " has recieved a response.");
             LOG.info("API response message-> "+swiftFundingResponse.getErrorDescription());
-        } catch (Exception e) {
+        }
+        catch(HttpClientErrorException e) {
+            if (e.getMessage().contains("Unauthorized")) {
+                LOG.error(String.format("The credentials to call the Flexpay Funding API is incorrect", otpServiceUrl), e.getMessage());
+            } else {
+                LOG.error(String.format("Flexpay Funding API is unavailable", otpServiceUrl), e.getMessage());
+            }
+            swiftFundingResponse = new SwiftFundingResponse(swiftInFundingRequest.getUniqueEndToEndIdentifier(), "DS28", "Return for Technical Reason");
+        }
+        catch (Exception e) {
             swiftFundingResponse = new SwiftFundingResponse(swiftInFundingRequest.getUniqueEndToEndIdentifier(),"DS28","Return for Technical Reason");
             LOG.error(String.format("Error calling service[%s]", otpServiceUrl), e.getMessage());
         }
